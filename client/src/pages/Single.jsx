6b7a1f2f075fda9aa8c57e9ab92baf6bddd0aab3
mchau98 <nguynminhchau98@gmail.com>
2024-07-16 23:45:09 +0700

    add write and post

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,5 +1,31 @@
 import React from "react";
+import Edit from "../img/edit.png";
+import Delete from "../img/delete.png";
+import { Link } from "react-router-dom";
+import Menu from "../components/Menu";
 const Single = () => {
-  return <div>Single</div>;
+  return (
+    <div className="single">
+      <div className="content">
+        <img src="https://images.pexels.com/photos/7058097/pexels-photo-7058097.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
+        <div className="user">
+          <img src="https://images.pexels.com/photos/5704849/pexels-photo-5704849.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
+          <div className="info">
+            <span>Josh</span>
+            <p>Posted 2 days ago</p>
+          </div>
+          <div className="edit">
+            <Link to={`/write?edit=2`}>
+              <img src={Edit} alt="" />
+            </Link>
+            <img src={Delete} alt="" />
+          </div>
+        </div>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit</h1>
+        <p>abc</p>
+      </div>
+      <Menu />
+    </div>
+  );
 };
 export default Single;
