1975a428818fed5af585ac0e97b573cdac81246a
mchau98 <nguynminhchau98@gmail.com>
2024-07-19 04:50:56 +0700

    update

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Logo from "../img/logo.png";
+import { CopyrightOutlined } from "@ant-design/icons";
 const Footer = () => {
   return (
     <footer>
-      <img src={Logo} />
-      <span>Made with reactjs</span>
+      <img src={Logo} alt="" />
+      <span>
+        <CopyrightOutlined />
+        <span className="copyright">copyright 2024</span>
+      </span>
     </footer>
   );
 };
