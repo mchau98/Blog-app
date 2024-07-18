ef286ce6d8caf791ec88c0cc384871074dbfe089
mchau98 <nguynminhchau98@gmail.com>
2024-07-19 00:51:21 +0700

    update db

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,25 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Edit from "../img/edit.png";
 import Delete from "../img/delete.png";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import Menu from "../components/Menu";
+import axios from "axios";
+import moment from "moment";
+import { useContext } from "react";
+import { AuthContext } from "../context/authContext";
+// import DOMPurify from "dompurify";
+
 const Single = () => {
+  const [post, setPost] = useState({});
+
+  const location = useLocation();
+
+  const postId = location.pathname.split("/")[2];
+
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/posts/${postId}`);
+        setPost(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, [postId]);
   return (
     <div className="single">
       <div className="content">
-        <img src="https://images.pexels.com/photos/7058097/pexels-photo-7058097.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
+        <img src={post?.img} />
         <div className="user">
           <img src="https://images.pexels.com/photos/5704849/pexels-photo-5704849.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
           <div className="info">
-            <span>Josh</span>
-            <p>Posted 2 days ago</p>
+            <span>{post.username}</span>
+            <p>Posted {moment(post.date).fromNow()}</p>
           </div>
-          <div className="edit">
-            <Link to={`/write?edit=2`}>
-              <img src={Edit} alt="" />
-            </Link>
-            <img src={Delete} alt="" />
-          </div>
+          {currentUser.username === post.username && (
+            <div className="edit">
+              <Link to={`/write?edit=2`} state={post}>
+                <img src={Edit} alt="" />
+              </Link>
+              <img src={Delete} alt="" />
+            </div>
+          )}
         </div>
         <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit</h1>
         <p>abc</p>
