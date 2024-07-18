025c3ca1ec6050aef3628067a218683698c8d197
mchau98 <nguynminhchau98@gmail.com>
2024-07-19 04:37:15 +0700

    add feature update post

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -71,21 +71,21 @@ export const deletePost = (req, res) => {
 
 export const updatePost = (req, res) => {
   res.json("from controller");
-  // const token = req.cookies.access_token;
-  // if (!token) return res.status(401).json("Not authenticated!");
+  const token = req.cookies.access_token;
+  if (!token) return res.status(401).json("Not authenticated!");
 
-  // jwt.verify(token, "jwtkey", (err, userInfo) => {
-  //   if (err) return res.status(403).json("Token is not valid!");
+  jwt.verify(token, "jwtkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid!");
 
-  //   const postId = req.params.id;
-  //   const q =
-  //     "UPDATE posts SET `title`=?,`desc`=?,`img`=?,`cat`=? WHERE `id` = ? AND `uid` = ?";
+    const postId = req.params.id;
+    const q =
+      "UPDATE posts SET `title`=?,`desc`=?,`img`=?,`cat`=? WHERE `id` = ? AND `uid` = ?";
 
-  //   const values = [req.body.title, req.body.desc, req.body.img, req.body.cat];
+    const values = [req.body.title, req.body.desc, req.body.img, req.body.cat];
 
-  //   db.query(q, [...values, postId, userInfo.id], (err, data) => {
-  //     if (err) return res.status(500).json(err);
-  //     return res.json("Post has been updated.");
-  //   });
-  // });
+    db.query(q, [...values, postId, userInfo.id], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.json("Post has been updated.");
+    });
+  });
 };
